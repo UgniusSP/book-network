eda0eeeac3c8ee74c263a4d504545fc427a8a270
ugnius.spokavicius <uspokavicius@gmail.com>
2024-11-28 18:11:05 +0200

    minor changes

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {CustomButton} from "../buttons/CustomButton";
-import {FaHome, FaRegUserCircle, FaSignOutAlt} from "react-icons/fa";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+import {Button} from "@mui/material";
 
 export const Header: React.FC = () => {
     const navigate = useNavigate();
@@ -26,17 +28,38 @@ export const Header: React.FC = () => {
     return (
         <header className="bg-white border text-black p-4 shadow">
             <div className="pl-5 container flex justify-between items-center">
-                <h1 onClick={handleHome} className="text-xl font-bold cursor-pointer text-sky-800">Book network</h1>
+                <h1 onClick={handleHome} className="text-xl font-semibold cursor-pointer text-sky-800">Book network</h1>
                 <nav className="flex items-center pr-5">
                     <ul className="flex items-center space-x-4">
                         <li>
-                            <CustomButton text="Add publication" onClick={handleAddPublication}/>
+                            <Button variant="outlined"
+                                    onClick={handleAddPublication}
+                                    sx={
+                                        {
+                                            color: 'sky-800',
+                                            borderColor: 'sky-800',
+                                            textTransform: 'none',
+                                            size: 'small'
+                                        }
+                                    }>
+                                Add publication
+                            </Button>
                         </li>
                         <li>
-                            <FaRegUserCircle onClick={handleProfile} size={30} className="cursor-pointer text-sky-800"/>
+                            <AccountCircleIcon onClick={handleProfile} sx={
+                                {
+                                    cursor: 'pointer',
+                                    fontSize: 25
+                                }
+                            }/>
                         </li>
                         <li>
-                            <FaSignOutAlt onClick={handleLogout} size={30} className="cursor-pointer text-sky-800"/>
+                            <LogoutIcon onClick={handleLogout} sx={
+                                {
+                                    cursor: 'pointer',
+                                    fontSize: 25
+                                }
+                            }/>
                         </li>
                     </ul>
                 </nav>
